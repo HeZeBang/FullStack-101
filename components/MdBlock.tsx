e057ae5426ff770ee39c20e665fc46fe19c6897e
zambar <zambar@shanghaitech.edu.cn>
2025-06-13 20:04:25 +0800

    fix: button and custom md

--- components/MdBlock.tsx
+++ components/MdBlock.tsx
@@ -1,9 +1,10 @@
+import { cn } from "@/lib/utils";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
-function MDBlock(props: { children: string }) {
+function MDBlock(props: { children: string, className?: string }) {
     return (
-        <div className="prose">
+        <div className={cn("prose", props.className)}>
             <Markdown remarkPlugins={[remarkGfm]}>
                 {props.children}
             </Markdown>
